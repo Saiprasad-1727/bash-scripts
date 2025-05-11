019126c199e08cd62d3bcf85a62482aba703de04
sai_prasad_shetty <87171068+Saiprasad-1727@users.noreply.github.com>
2025-05-11 20:49:16 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,18 @@
 (Reference URL for commands: https://www.jenkins.io/doc/book/installing/linux/#debianubuntu)
 #!/bin/bash
+#this Script belong to Cloudaseem Youtube channel #####
+# jenkins installation on ubuntu 
 sudo apt update -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt install fontconfig openjdk-17-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins -y 
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 
 # Verifiy Jenkins installation: jenkins --version
 
